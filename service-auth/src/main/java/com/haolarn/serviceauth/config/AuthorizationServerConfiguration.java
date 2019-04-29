07726e78bcecaf65edc83465c4c287d52718999b
haolarn <laizhihao@aimeinuo.com>
2019-04-29 14:37:26 +0800

    "修改了两个注解进行增强验证"

--- service-auth/src/main/java/com/haolarn/serviceauth/config/AuthorizationServerConfiguration.java
+++ service-auth/src/main/java/com/haolarn/serviceauth/config/AuthorizationServerConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 
 //作为配置bean注入spring容器
 @Configuration
